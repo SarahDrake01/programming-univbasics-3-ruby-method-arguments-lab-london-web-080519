f1971b052eb77a3740d1c365cd95922a1d38f3bc
Sarah Drake <jussaradrake@outlook.com>
2019-07-03 15:08:04 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -15,9 +15,9 @@ end
 
 introduction_with_language_optional("Edwin", "Ruby")
 
-
-describe "#introduction_with_language_optional" do
-  it "takes in two arguments, a name and a language, language can be optional or defined" do
-    expect{introduction_with_language_optional("Edwin", "Python")}.to output("Hi, my name is Edwin and I am learning to program in Python.\n").to_stdout
-  end
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
+
+introduction_with_language_optional("Edwin", "Python")
+
