fb37f2ea6cdf0dc2db7d317f9e52d143803976bb
Sarah Drake <jussaradrake@outlook.com>
2019-07-03 15:03:48 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -8,11 +8,12 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-describe "#introduction_with_language_optional" do
-  it "takes in two arguments, a name and a language, and language defaults to Ruby" do
-    expect{introduction_with_language_optional("Edwin")}.to output("Hi, my name is Edwin and I am learning to program in Ruby.\n").to_stdout
-  end
+
+def introduction_with_language_optional("Edwin", "Ruby")
+  puts "Hi my name is #{name} and I am learning to program in #{language}"
 end
+
+
 
 describe "#introduction_with_language_optional" do
   it "takes in two arguments, a name and a language, language can be optional or defined" do
